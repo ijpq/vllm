b6aa71b5e84bfc851d23bb4386dee283b77253c8
ijpq <509634578tk@gmail.com>
2026-01-07 17:42:19 +0800

    add dispatch wrapper

--- csrc/moe/fused_routing_kernels.cu
+++ csrc/moe/fused_routing_kernels.cu
@@ -41,12 +41,14 @@ __global__ void fused_routing_kernel(
 
 template <>
 __global__ void fused_routing_kernel<32, 4, 4>(
-    __nv_bfloat16* __restrict__ topk_weights,
-    int16_t* __restrict__ topk_indices, int64_t max_n_tiles, int64_t NUM_TOKENS,
-    __nv_bfloat16* __restrict__ gate_scale, int32_t* __restrict__ topk_index,
-    int32_t* __restrict__ gate_index, int32_t* __restrict__ token_offs_pad_ptr,
+    const __nv_bfloat16* __restrict__ topk_weights,
+    const int16_t* __restrict__ topk_indices, const int64_t max_n_tiles,
+    const int64_t NUM_TOKENS, __nv_bfloat16* __restrict__ gate_scale,
+    int32_t* __restrict__ topk_index, int32_t* __restrict__ gate_index,
+    int32_t* __restrict__ token_offs_pad_ptr,
     int32_t* __restrict__ block_pid_map_ptr,
     int32_t* __restrict__ expt_offs_ptr, int32_t* __restrict__ hist_ptr) {
+    static_assert(alignof(topk_indices) == 128);
     namespace cg = cooperative_groups;
     cg::cluster_group cluster = cg::this_cluster();
 
@@ -54,9 +56,9 @@ __global__ void fused_routing_kernel<32, 4, 4>(
     int tid = local_tid + blockDim.x * blockIdx.x;
     int CTA_ID = blockIdx.x;
     int num_threads = blockDim.x * gridDim.x;
-    const int NUM_EXPERTS = 32;
-    const int topk = 4;
-    constexpr int NUM_BLOCK_SIZES = 4;
+    // const int NUM_EXPERTS = 32;
+    // const int topk = 4;
+    // constexpr int NUM_BLOCK_SIZES = 4;
     int ROWS_PER_THREADS = (NUM_TOKENS + num_threads - 1) / num_threads;
     int ROWS_PER_CTA = (NUM_TOKENS + gridDim.x - 1) / gridDim.x;
 
@@ -99,7 +101,9 @@ __global__ void fused_routing_kernel<32, 4, 4>(
     int row = CTA_ID * ROWS_PER_CTA;
     int row_stride = ROWS_PER_THREADS * blockDim.x;
 #pragma unroll
-    for (int i = row + local_tid; i < NUM_TOKENS; i += row_stride) {
+    for (int i = row + local_tid; i < NUM_TOKENS;
+         i += row_stride) {  // mem transaction = warp_size * topk *
+                             // sizeof(topk_indices)
         int64_t row_experts =
             *reinterpret_cast<int64_t*>(topk_indices + tid * topk);
         auto expt0 = static_cast<int32_t>(row_experts & 0xFFFF);
@@ -117,35 +121,36 @@ __global__ void fused_routing_kernel<32, 4, 4>(
 #pragma unroll
     if (local_tid < NUM_EXPERTS) {
         atomicAdd(global_hist + local_tid, local_hist[local_tid]);
-        printf("tid: %d, CTA: %d", local_tid, blockIdx.x);
     }
     cluster.sync();
 
     /* phase 2*/
     int warp_id = threadIdx.x / 32;
-    if (blockIdx.x == 0 && warp_id < NUM_BLOCK_SIZES) {
+    // compute expert_across_prefixsum, dst_experts[i] =
+    // sum_{p=0}^{j-1}{local_hist[p]}, where i is expert_id, j is CTA_ID
+    // TODO(ijpq): we can leverage warpscan to improve this process, but need
+    // assign warp threads into [NUM_CTAS, NUM_EXPERTS] carefully.
+    if (CTA_ID == 0 && local_tid < NUM_EXPERTS) {
+        for (int bidx = 1; bidx < gridDim.x; bidx++) {
+            int32_t* dst_experts = cluster.map_shared_rank(
+                reinterpret_cast<int32_t*>(sm_hist + expert_across_offset),
+                bidx);
+            int32_t* tb_local_hist = cluster.map_shared_rank(
+                reinterpret_cast<int32_t*>(sm_hist + local_hist_offset),
+                bidx - 1);
+            int32_t* accum_hist = cluster.map_shared_rank(
+                reinterpret_cast<int32_t*>(sm_hist + expert_across_offset),
+                bidx - 1);
+            dst_experts[local_tid] =
+                tb_local_hist[local_tid] + accum_hist[local_tid];
+        }
+    }
+    if (CTA_ID == 0 && warp_id < NUM_BLOCK_SIZES) {
         int lane_id = threadIdx.x % 32;
         int h =
             global_hist[lane_id];  // how many tokens are routed to this
                                    // expert, globally. let's say [100,20,50]
-        if (local_tid < 32) {
-            // compute expert_across_prefixsum, dst_experts[i] =
-            // sum_p_0_j-1(local_hist[p]), where i is expert_id, j is tb's id
-            // XXX(ijpq): we may leverage warpscan to improve this process
-            for (int bidx = 1; bidx < gridDim.x; bidx++) {
-                int32_t* dst_experts = cluster.map_shared_rank(
-                    reinterpret_cast<int32_t*>(sm_hist + expert_across_offset),
-                    bidx);
-                int32_t* tb_local_hist = cluster.map_shared_rank(
-                    reinterpret_cast<int32_t*>(sm_hist + local_hist_offset),
-                    bidx - 1);
-                int32_t* accum_hist = cluster.map_shared_rank(
-                    reinterpret_cast<int32_t*>(sm_hist + expert_across_offset),
-                    bidx - 1);
-                dst_experts[local_tid] =
-                    tb_local_hist[local_tid] + accum_hist[local_tid];
-            }
-
+        if (local_tid < NUM_EXPERTS) {
             int exclusive_res = 0;
             int warp_reduce = 0;
             WarpScan(temp_storage[warp_id])
@@ -154,7 +159,7 @@ __global__ void fused_routing_kernel<32, 4, 4>(
             int32_t* hist_sum = reinterpret_cast<int32_t*>(
                 sm_hist + global_hist_exclusivesum_offset);
             hist_sum[local_tid] = exclusive_res;
-            if (threadIdx.x == 0) hist_sum[32] = warp_reduce;
+            if (local_tid == 0) hist_sum[NUM_EXPERTS] = warp_reduce;
         }
 
         // compute global hist exclusive sum
@@ -211,23 +216,26 @@ __global__ void fused_routing_kernel<32, 4, 4>(
             pid_map_row[(tile_start + block_idx)] = packed_val;
         }
     }
-
     cluster.sync();
+
     // WB global memory(pidmap, tokenoffspad, global hist sum)
     int token_offs_pad_size = NUM_BLOCK_SIZES * (NUM_EXPERTS + 1);
     int pid_map_size = NUM_BLOCK_SIZES * (max_n_tiles);
-    int32_t* token_offs_pad = cluster.map_shared_rank(
-        reinterpret_cast<int32_t*>(sm_hist + token_offs_pad_offset), 0);
-    int32_t* block_pid = cluster.map_shared_rank(
-        reinterpret_cast<int32_t*>(sm_hist + block_pid_offset), 0);
+    // int32_t* token_offs_pad = cluster.map_shared_rank(
+    //     reinterpret_cast<int32_t*>(sm_hist + token_offs_pad_offset), 0);
+    int32_t* token_offs_pad =
+        reinterpret_cast<int32_t*>(sm_hist + token_offs_pad_offset);
+    // int32_t* block_pid = cluster.map_shared_rank(
+    //     reinterpret_cast<int32_t*>(sm_hist + block_pid_offset), 0);
+    int32_t* block_pid = reinterpret_cast<int32_t*>(sm_hist + block_pid_offset);
 
-    if (blockIdx.x == 0) {
+    if (CTA_ID == 0) {
         for (int i = local_tid; i < token_offs_pad_size; i += blockDim.x)
             token_offs_pad_ptr[i] = token_offs_pad[i];
         for (int i = local_tid; i < pid_map_size; i += blockDim.x)
             block_pid_map_ptr[i] = block_pid[i];
     }
-    if (blockIdx.x == 0 && local_tid < 32) {
+    if (CTA_ID == 0 && local_tid < NUM_EXPERTS) {
         int32_t* hist_sum = reinterpret_cast<int32_t*>(
             sm_hist + global_hist_exclusivesum_offset);
         int32_t* global_hist_sm =
@@ -236,6 +244,7 @@ __global__ void fused_routing_kernel<32, 4, 4>(
         if (local_tid == 0) expt_offs_ptr[NUM_EXPERTS] = hist_sum[NUM_EXPERTS];
         hist_ptr[local_tid] = global_hist_sm[local_tid];
     }
+    cluster.sync();
 
     /* phase 3*/
 
@@ -267,17 +276,25 @@ __global__ void fused_routing_kernel<32, 4, 4>(
 }  // namespace moe
 }  // namespace vllm
 
-void fused_routing(torch::Tensor& gating_output, torch::Tensor& topk_weights,
-                   torch::Tensor& topk_indices, int64_t max_n_tiles,
-                   int64_t topk, torch::Tensor& gate_scale,
-                   torch::Tensor& topk_index, torch::Tensor& gate_index,
-                   torch::Tensor& token_offs_pad, torch::Tensor& block_pid_map,
-                   torch::Tensor& expt_offs, torch::Tensor& hist) {
+template <typename ValType>
+void routing_kernel_helper(torch::Tensor& topk_weights,
+                           torch::Tensor& topk_indices, int64_t max_n_tiles,
+                           int64_t topk,
+
+                           torch::Tensor& gate_scale, torch::Tensor& topk_index,
+                           torch::Tensor& gate_index,
+                           torch::Tensor& token_offs_pad,
+                           torch::Tensor& block_pid_map,
+                           torch::Tensor& expt_offs, torch::Tensor& hist) {
+    /*
+    dispatch config
+    */
     TORCH_CHECK(topk == 4, "");
     constexpr int const_topk = 4;
     constexpr int NUM_BLOCK_SIZES = 4;
     const auto num_experts = gating_output.size(-1);
     const auto num_tokens = gating_output.numel() / num_experts;
+    
     switch (num_experts) {
         case 32: {
             auto warp_size = 32;
@@ -289,9 +306,6 @@ void fused_routing(torch::Tensor& gating_output, torch::Tensor& topk_weights,
                                                   NUM_BLOCK_SIZES>);
 
             auto hypo_cluster_size = 8;
-            // auto hypo_num_threads = hypo_cluster_size * THREAD_PER_CTA;
-            // auto hypo_rows_per_threads =
-            //     (num_tokens + hypo_num_threads - 1) / hypo_num_threads;
             size_t global_hist_size = num_experts;
             size_t local_hist_size = num_experts;
             size_t global_hist_prefix_size = num_experts + 1;
@@ -328,9 +342,9 @@ void fused_routing(torch::Tensor& gating_output, torch::Tensor& topk_weights,
             config.numAttrs = 1;
 
             auto topk_weights_ptr =
-                reinterpret_cast<__nv_bfloat16*>(topk_weights.data_ptr());
+                reinterpret_cast<ValType*>(topk_weights.data_ptr());
             auto gate_scale_ptr =
-                reinterpret_cast<__nv_bfloat16*>(gate_scale.data_ptr());
+                reinterpret_cast<ValType*>(gate_scale.data_ptr());
             auto topk_indices_ptr = topk_indices.data_ptr<int16_t>();
             auto topk_index_ptr = topk_index.data_ptr<int32_t>();
             auto gate_index_ptr = gate_index.data_ptr<int32_t>();
@@ -346,11 +360,27 @@ void fused_routing(torch::Tensor& gating_output, torch::Tensor& topk_weights,
                                expt_offs_ptr, hist_ptr);
             break;
         }
-        case 128: {
-            break;
-        }
-        default: {
-            TORCH_CHECK(false, "Unsupported num experts: ", num_experts);
-        }
+        TORCH_CHECK(false, "Unsupported num experts: ", num_experts);
+    }
+}
+
+void fused_routing(torch::Tensor& gating_output, torch::Tensor& topk_weights,
+                   torch::Tensor& topk_indices, int64_t max_n_tiles,
+                   int64_t topk, torch::Tensor& gate_scale,
+                   torch::Tensor& topk_index, torch::Tensor& gate_index,
+                   torch::Tensor& token_offs_pad, torch::Tensor& block_pid_map,
+                   torch::Tensor& expt_offs, torch::Tensor& hist) {
+    /*
+    dispatch dtype
+    */
+    if (topk_indices.scalar_type() == at::ScalarType::BFloat16 &&
+        gate_scale.scalar_type() == at::ScalarType::BFloat16) {
+        routing_kernel_helper<__nv_bfloat16>(
+            topk_weights, topk_indices, max_n_tiles, topk, gate_scale,
+            topk_index, gate_index, token_offs_pad, block_pid_map, expt_offs,
+            hist);
+    } else {
+        TORCH_CHECK(false, "Unsupported dtype: ", topk_indices.scalar_type(),
+                    gate_scale.scalar_type());
     }
 }
